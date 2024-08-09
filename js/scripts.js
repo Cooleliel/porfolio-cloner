629ab97b5ca58e3de4c58f12db21034b3ae450d1
Cooleliel <elielcoulibaly39@gmail.com>
2024-08-09 10:20:18 +0000

    gérer le scroll de la page et la section active avec js

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+/** toggle icon navbar*/
+
+/** scroll sections */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+ sections.forEach(section => {
+  let top = window.scrollY;
+  let offset = section.offsetTop - 100;
+  let height = section.offsetHeight;
+  let id = section.getAttribute('id');
+
+  if(top >= offset && top < offset + height) {
+   //active navbar links 
+   navLinks.forEach(links => {
+    links.classList.remove('active');
+    document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+   });
+  }
+ });
+
+ //sticky header
+ let header = document.querySelector('header');
+
+ header.classList.toggle('sticky', window.scrollY > 100);
+}
+
