75f89a7c700561b977f4db2b88c736610b7e096f
Cooleliel <elielcoulibaly39@gmail.com>
2024-08-12 11:17:28 +0000

    appliquer les animations lors du scroll

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,14 @@ window.onscroll = () => {
     links.classList.remove('active');
     document.querySelector('header nav a[href*=' + id +']').classList.add('active');
    });
+
+   //active sections for animation on scroll
+   section.classList.add('show-animate');
+  }
+
+  //if want to use animation that repeats on scroll use this
+  else {
+   section.classList.remove('show-animate')
   }
  });
 
